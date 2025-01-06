e96fd5dad7ec9c49f5dea6fd04435b1f26074ea7
Dilum Aluthge <dilum@aluthge.com>
2025-01-05 20:37:20 -0500

    Delete all non-Slurm tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,39 +9,17 @@ using Distributed: workers, nworkers
 using Distributed: procs, nprocs
 using Distributed: remotecall_fetch, @spawnat
 using Test: @testset, @test, @test_skip
-# ElasticManager:
-using ClusterManagers: ElasticManager
-# Slurm:
-using ClusterManagers: addprocs_slurm, SlurmManager
+
 # LSF:
 using ClusterManagers: addprocs_lsf, LSFManager
-# SGE:
-using ClusterManagers: addprocs_sge, SGEManager
 
 const test_args = lowercase.(strip.(ARGS))
 
 @info "" test_args
 
-slurm_is_installed() = !isnothing(Sys.which("sbatch"))
 lsf_is_installed() = !isnothing(Sys.which("bsub"))
-qsub_is_installed() = !isnothing(Sys.which("qsub"))
 
 @testset "ClusterManagers.jl" begin
-    include("elastic.jl")
-    
-    if slurm_is_installed()
-        @info "Running the Slurm tests..." Sys.which("sbatch")
-        include("slurm.jl")
-    else
-        if "slurm" in test_args
-            @error "ERROR: The Slurm tests were explicitly requested in ARGS, but sbatch was not found, so the Slurm tests cannot be run" Sys.which("sbatch") test_args
-            @test false
-        else
-            @warn "sbatch was not found - Slurm tests will be skipped" Sys.which("sbatch")
-            @test_skip false
-        end
-    end
-    
     if lsf_is_installed()
         @info "Running the LSF tests..." Sys.which("bsub")
         include("lsf.jl")
@@ -54,18 +32,4 @@ qsub_is_installed() = !isnothing(Sys.which("qsub"))
             @test_skip false
         end
     end
-    
-    if qsub_is_installed()
-        @info "Running the SGE (via qsub) tests..." Sys.which("qsub")
-        include("slurm.jl")
-    else
-        if "sge_qsub" in test_args
-            @error "ERROR: The SGE tests were explicitly requested in ARGS, but qsub was not found, so the SGE tests cannot be run" Sys.which("qsub") test_args
-            @test false
-        else
-            @warn "qsub was not found - SGE tests will be skipped" Sys.which("qsub")
-            @test_skip false
-        end
-    end
-
 end # @testset
