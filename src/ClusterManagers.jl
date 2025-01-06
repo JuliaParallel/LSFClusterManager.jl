cf80206626b74bb9f0d817ae41bc9e0e4946db40
Dilum Aluthge <dilum@aluthge.com>
2025-01-05 20:38:38 -0500

    Remove all non-LSF source code files (in the `src/` directory)

--- src/ClusterManagers.jl
+++ src/ClusterManagers.jl
@@ -10,15 +10,6 @@ import Distributed: launch, manage, kill, init_worker, connect
 worker_cookie() = begin Distributed.init_multi(); cluster_cookie() end
 worker_arg() = `--worker=$(worker_cookie())`
 
-
-# PBS doesn't have the same semantics as SGE wrt to file accumulate,
-# a different solution will have to be found
-include("qsub.jl")
-include("scyld.jl")
-include("condor.jl")
-include("slurm.jl")
-include("affinity.jl")
-include("elastic.jl")
 include("lsf.jl")
 
 end
