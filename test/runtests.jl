1a2364375c2a4516a56a2563d747596ec91562f9
Dilum Aluthge <dilum@aluthge.com>
2025-02-02 22:05:29 -0500

    Simplify the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,23 +13,12 @@ using Test: @testset, @test, @test_skip
 # LSF:
 using LSFClusterManager: addprocs_lsf, LSFManager
 
-const test_args = lowercase.(strip.(ARGS))
-
-@info "" test_args
-
 lsf_is_installed() = !isnothing(Sys.which("bsub"))
 
 @testset "LSFClusterManager.jl" begin
-    if lsf_is_installed()
-        @info "Running the LSF tests..." Sys.which("bsub")
-        include("lsf.jl")
-    else
-        if "lsf" in test_args
-            @error "ERROR: The LSF tests were explicitly requested in ARGS, but bsub was not found, so the LSF tests cannot be run" Sys.which("bsub") test_args
-            @test false
-        else
-            @warn "bsub was not found - LSF tests will be skipped" Sys.which("bsub")
-            @test_skip false
-        end
-    end
-end # @testset
+    @info "" Sys.which("bsub")
+
+    @test lsf_is_installed()
+
+    include("lsf.jl")
+end
